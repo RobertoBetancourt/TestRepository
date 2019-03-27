c56b66ee4b9052c9d5f91dea89736a25adebb712
RobertoBetancourt <rob_betancourt@hotmail.com>
2019-03-27 09:31:47 -0600

    Cambio Roberto

--- Hello.java
+++ Hello.java
@@ -1,6 +1,6 @@
 class Hello {
     public static void main(String[] args) {
-        for (int i = 0; i < 1000; i++) {
+        for (int i = 0; i < 10000; i++) {
             System.out.println("Hola, Roberto");
         }
     }
