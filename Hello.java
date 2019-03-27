62f1877234c5fa69640118a6de97a61aadcc19f2
Miguel <miguelsilva_ramos15@live.com.mx>
2019-03-27 09:30:06 -0600

    Este es mi cambio

--- Hello.java
+++ Hello.java
@@ -1,6 +1,6 @@
 class Hello {
     public static void main(String[] args) {
-        for (int i = 0; i < 1000; i++) {
+        for (int i = 0; i < 500; i++) {
             System.out.println("Hola, Roberto");
         }
     }
